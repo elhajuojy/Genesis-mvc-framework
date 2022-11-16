bf194c3e8488d02a87b7a3d18099967be45bc1df
elhajuojy <souirimehdi311@gmail.com>
2022-11-16 14:58:18 +0100

    create folders for the framework and implament basic routing systeme

---
+++ core/Router.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\core;
+
+class Router
+{
+    protected  array  $routes=[];
+    public  Request $request;
+
+    /**
+     * @param Request $request
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
+
+    public  function get($path,$callback){
+        $this->routes['get'][$path]=$callback;
+    }
+
+    public function resolve()
+    {
+        $path =  $this->request->getPath();
+        $method = $this->request->getMethod();
+        $callback = $this->routes[$method][$path]??false;
+        if (!$callback){
+            echo "Not found ";
+            exit();
+        }
+       echo  call_user_func($callback);
+    }
+
+}
